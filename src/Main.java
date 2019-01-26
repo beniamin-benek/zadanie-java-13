7a9b8cfad1f0f4c79aab6d89acfd7d5201880f8b
benek <ben.lyzn@wp.pl>
2019-01-26 01:43:37 +0100

    Rozwiązanie.

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int number;
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            System.out.print("Podaj liczbę: ");
+            number = scanner.nextInt();
+            showAlert(number);
+        } while (!(number > 100 && number < 200 && number % 3 == 0));
+
+    }
+
+    private static void showAlert(int number) {
+        if (number < 100)
+            System.out.println("Podana liczba jest za mała.");
+        else if (number > 200)
+            System.out.println("Podana liczba jest za duża.");
+        else if (!(number % 3 == 0))
+            System.out.println("Podana liczba nie jest podzielna przez 3.");
+    }
+
+}
